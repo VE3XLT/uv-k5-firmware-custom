74dfe4231a0d570f5b945f75ed31f4490677c3aa
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-01-14 18:07:24 +0100

    Change version

--- version.c
+++ version.c
@@ -5,10 +5,11 @@
 	#define VER     ""
 #endif
 
-
-const char Version[]      = AUTHOR_STRING VER;
-const char UART_Version[] = "UV-K5 Firmware, Open Edition, " AUTHOR_STRING VER "\r\n";
-
 #ifdef ENABLE_FEAT_F4HWN
+	const char Version[]      = AUTHOR_STRING " v0.21+";
+	const char UART_Version[] = "UV-K5 Firmware, Open Edition, " AUTHOR_STRING "\r\n";
 	const char Feat[]         = "Feat " FEAT_STRING;
+#else
+	const char Version[]      = AUTHOR_STRING VER;
+	const char UART_Version[] = "UV-K5 Firmware, Open Edition, " AUTHOR_STRING VER "\r\n";
 #endif
