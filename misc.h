829517fb2bc1c1e495f6778235a03e7801b79fa2
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-01-17 00:15:00 +0100

    Fix tab

--- misc.h
+++ misc.h
@@ -209,7 +209,7 @@ extern volatile bool         gNextTimeslice_500ms;
 extern volatile uint16_t     gTxTimerCountdown_500ms;
 extern volatile bool         gTxTimeoutReached;
 #ifdef ENABLE_FEAT_F4HWN
-	extern volatile uint16_t 			gTxTimerCountdownAlert_500ms;
+	extern volatile uint16_t 	gTxTimerCountdownAlert_500ms;
 	extern volatile bool        gTxTimeoutReachedAlert;
 #endif
 
