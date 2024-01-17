236c7eae06dd2aecc873324c90a732d388f564c1
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-01-17 20:03:46 +0100

    Refactoring shell script

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -2,10 +2,5 @@
 
 IMAGE_NAME="uvk5"
 
-if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
-    echo "Image exists locally"
-else
-	docker build -t $IMAGE_NAME .
-fi
-
+docker build -t $IMAGE_NAME .
 docker run --rm -v "${PWD}/compiled-firmware:/app/compiled-firmware" $IMAGE_NAME /bin/bash -c "cd /app && make && cp firmware* compiled-firmware/"
