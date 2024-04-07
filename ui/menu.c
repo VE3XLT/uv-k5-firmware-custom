58bdcfb92224e75709f64e35e16ac109437e7fb6
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-04-07 19:08:29 +0200

    Shift index

--- ui/menu.c
+++ ui/menu.c
@@ -518,10 +518,11 @@ void UI_DisplayMenu(void)
 			// draw the menu index number/count
 #ifdef ENABLE_FEAT_F4HWN
 			sprintf(String, "%02u/%u", 1 + gMenuCursor, gMenuListCount);
+			UI_PrintStringSmallNormal(String, 6, 0, 6);
 #else
 			sprintf(String, "%2u.%u", 1 + gMenuCursor, gMenuListCount);
-#endif
 			UI_PrintStringSmallNormal(String, 2, 0, 6);
+#endif
 		}
 		else if (menu_index >= 0 && menu_index < (int)gMenuListCount)
 		{	// current menu item
