b96138687f30e3c83bac45e6f533bc8978804944
prokrypt <prokrypt@edited.us>
2024-05-26 01:51:16 +0000

    button hold save stuff all fixed!

--- app/app.c
+++ app/app.c
@@ -2002,18 +2002,24 @@ Skip:
 #ifdef ENABLE_FMRADIO
 	if (gRequestSaveFM) {
 		gRequestSaveFM = false;
-		gFlagSaveFM = true;
+		if (!bKeyHeld && !bKeyPressed)
+			SETTINGS_SaveFM();
+		else
+			gFlagSaveFM = true;
 	}
 #endif
 
 	if (gRequestSaveVFO) {
 		gRequestSaveVFO = false;
-		flagSaveVfo = true;
+		if (!bKeyHeld && !bKeyPressed)
+			SETTINGS_SaveVfoIndices();
+		else
+			flagSaveVfo = true;
 	}
 
 	if (gRequestSaveChannel > 0) { // TODO: remove the gRequestSaveChannel, why use global variable for that??
-		if (!bKeyHeld) {
-//			SETTINGS_SaveChannel(gTxVfo->CHANNEL_SAVE, gEeprom.TX_VFO, gTxVfo, gRequestSaveChannel);
+		if (!bKeyHeld && !bKeyPressed) {
+			SETTINGS_SaveChannel(gTxVfo->CHANNEL_SAVE, gEeprom.TX_VFO, gTxVfo, gRequestSaveChannel);
 
 			if (!SCANNER_IsScanning() && gVfoConfigureMode == VFO_CONFIGURE_NONE)
 				// gVfoConfigureMode is so as we don't wipe out previously setting this variable elsewhere
