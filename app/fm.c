4bdf62f13878ff7fd9c40b08a59f8096449ffa32
prokrypt <prokrypt@edited.us>
2024-06-19 21:40:47 -0700

    Update fm.c: tune by .2

--- app/fm.c
+++ app/fm.c
@@ -531,10 +531,10 @@ void FM_ProcessKeys(KEY_Code_t Key, bool bKeyPressed, bool bKeyHeld)
 			Key_MENU(state);
 			break;
 		case KEY_UP:
-			Key_UP_DOWN(state, 1);
+			Key_UP_DOWN(state, 2);
 			break;
 		case KEY_DOWN:
-			Key_UP_DOWN(state, -1);
+			Key_UP_DOWN(state, -2);
 			break;;
 		case KEY_EXIT:
 			Key_EXIT(state);
