269caa7565e3b6fa8dfa1d00c31257f497a6e00e
prokrypt <prokrypt@edited.us>
2024-06-22 15:39:44 -0700

    Update menu.c: mV

--- ui/menu.c
+++ ui/menu.c
@@ -869,16 +869,16 @@ void UI_DisplayMenu(void)
 
 		case MENU_VOL:
 #ifdef ENABLE_FEAT_F4HWN
-			sprintf(String, "%u.%02uV %u%%\n%s\n%s",
-				gBatteryVoltageAverage / 100, gBatteryVoltageAverage % 100,
-				BATTERY_VoltsToPercent(gBatteryVoltageAverage),
+			sprintf(String, "%u.%03uV\n%u%% %s\n%s",
+				gBatteryVoltageAverage / 1000, gBatteryVoltageAverage % 1000,
+				gBatteryPercent,
 				AUTHOR_STRING_2,
 				VERSION_STRING_2
 				);
 #else
-			sprintf(String, "%u.%02uV\n%u%%",
-				gBatteryVoltageAverage / 100, gBatteryVoltageAverage % 100,
-				BATTERY_VoltsToPercent(gBatteryVoltageAverage));
+			sprintf(String, "%u.%03uV\n%u%%",
+				gBatteryVoltageAverage / 1000, gBatteryVoltageAverage % 1000,
+				gBatteryPercent);
 #endif
 			break;
 
