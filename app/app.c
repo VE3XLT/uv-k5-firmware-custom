951522bb3977e9b2e2cb2c910f057304169d7e20
prokrypt <prokrypt@edited.us>
2024-07-01 14:50:18 -0700

    vfo double eeprom write fix

--- app/app.c
+++ app/app.c
@@ -2029,7 +2029,7 @@ Skip:
 	}
 
 	if (gRequestSaveChannel > 0) { // TODO: remove the gRequestSaveChannel, why use global variable for that??
-		if (!bKeyHeld) {
+		if (!bKeyHeld && !bKeyPressed) {
 			SETTINGS_SaveChannel(gTxVfo->CHANNEL_SAVE, gEeprom.TX_VFO, gTxVfo, gRequestSaveChannel);
 
 			if (!SCANNER_IsScanning() && gVfoConfigureMode == VFO_CONFIGURE_NONE)
