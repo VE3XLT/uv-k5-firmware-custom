ab62ef040cb6da43e06ca466e6dc65fcd7313002
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-07-10 05:38:10 +0200

    Fix issue #180

--- frequencies.c
+++ frequencies.c
@@ -230,11 +230,8 @@ int32_t TX_freq_check(const uint32_t Frequency)
 
 		case F_LOCK_GMRS_FRS_MURS:
 			// https://forums.radioreference.com/threads/the-great-unofficial-radioreference-frs-gmrs-murs-fact-sheet.275370/
-			if (Frequency >= 46256250 && Frequency <= 46271250) // FRS/GMRS
-				return 0;
-			if (Frequency >= 46756250 && Frequency <= 46771250) // FRS ONLY
-				return 0;
-			if (Frequency >= 46255000 && Frequency <= 46272500) // GMRS ONLY
+			if ((Frequency >= 46255000 && Frequency <= 46272500) ||
+				(Frequency >= 46755000 && Frequency <= 46772500)) // FRS/GMRS
 				return 0;
 			if (Frequency == 15182000 || 
 				Frequency == 15188000 || 
