e731d9d1f94f79e1a50adae79f89d7e3f74b566b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-07-10 16:30:05 +0200

    Fix issue #182

--- app/main.c
+++ app/main.c
@@ -236,8 +236,8 @@ static void processFKeyFunction(const KEY_Code_t Key, const bool beep)
 		case KEY_7:
 #ifdef ENABLE_VOX
 			ACTION_Vox();
-#else
-			toggle_chan_scanlist();
+//#else
+//			toggle_chan_scanlist();
 #endif
 			break;
 
