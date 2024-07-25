ec2c470a902a3cf35217ca6e9d7b78c37f75ea40
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-07-25 17:01:59 +0200

    Fixed a bug when loading SList parameter, if ALL had been saved

--- settings.c
+++ settings.c
@@ -225,7 +225,7 @@ void SETTINGS_InitEEPROM(void)
 
 	// 0F18..0F1F
 	EEPROM_ReadBuffer(0x0F18, Data, 8);
-	gEeprom.SCAN_LIST_DEFAULT = (Data[0] < 5) ? Data[0] : 0;  // we now have 'all' channel scan option
+	gEeprom.SCAN_LIST_DEFAULT = (Data[0] < 6) ? Data[0] : 0;  // we now have 'all' channel scan option
 
 	// Fake data
 	/*
