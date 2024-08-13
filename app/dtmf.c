efdea646b7290cc125cb37efba204a27e6d02929
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-08-13 04:37:44 +0200

    Save 16 bytes

--- app/dtmf.c
+++ app/dtmf.c
@@ -171,18 +171,11 @@ bool DTMF_FindContact(const char *pContact, char *pResult)
 
 char DTMF_GetCharacter(const unsigned int code)
 {
+	if (code <= KEY_9)
+		return '0' + code;
+
 	switch (code)
 	{
-		case KEY_0:    return '0';
-		case KEY_1:    return '1';
-		case KEY_2:    return '2';
-		case KEY_3:    return '3';
-		case KEY_4:    return '4';
-		case KEY_5:    return '5';
-		case KEY_6:    return '6';
-		case KEY_7:    return '7';
-		case KEY_8:    return '8';
-		case KEY_9:    return '9';
 		case KEY_MENU: return 'A';
 		case KEY_UP:   return 'B';
 		case KEY_DOWN: return 'C';
